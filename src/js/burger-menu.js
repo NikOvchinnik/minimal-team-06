2e9f01ac43213a4f1b02bcdde08947ceec5191d3
NikOvchinnik <n.ovchinnik.s@gmail.com>
2023-08-14 21:53:33 +0300

    header nav

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -3,10 +3,17 @@
     openBurgerBtn: document.querySelector('[data-burger-open]'),
     closeBurgerBtn: document.querySelector('[data-burger-close]'),
     burger: document.querySelector('[data-burger]'),
+    navigationLinks: document.querySelectorAll('.navigation a'),
   };
 
   refs.openBurgerBtn.addEventListener('click', toggleBurger);
   refs.closeBurgerBtn.addEventListener('click', toggleBurger);
+
+   refs.navigationLinks.forEach(link => {
+     link.addEventListener('click', () => {
+       toggleBurger();
+     });
+   });
 
   function toggleBurger() {
     refs.burger.classList.toggle('is-hidden');
